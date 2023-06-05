201f40dd034498a99b96f2ffb2aff68a5d00a421
Patric Stout <truebrain@openttd.org>
2023-06-05 16:44:25 +0200

    chore: script to deploy all stacks

---
+++ deploy-all.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+cd $(dirname $0)
+
+( cd global-config && pulumi up -r -s prod )
+( cd aws-core && pulumi up -r -s prod )
+# Refreshing cloudflare seems not possible.
+( cd cloudflare-core && pulumi up -s prod )
+( cd nomad-core && pulumi up -r -s prod )
+
+APP_LIST="
+website
+wiki
+"
+
+IFS="
+"
+
+for app in ${APP_LIST}; do
+    if [ -e app/${app}/Pulumi.preview.yaml ]; then
+        ( cd app/${app} && pulumi up -r -s preview )
+    fi
+
+    ( cd app/${app} && pulumi up -r -s prod )
+done
