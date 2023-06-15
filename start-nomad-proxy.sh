a28a870eb0c4f2b8c3628ef406fc621f16ad0ee5
Patric Stout <truebrain@openttd.org>
2023-06-15 11:10:59 +0200

    fix: make sure start-nomad-proxy actually works

--- start-nomad-proxy.sh
+++ start-nomad-proxy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-(
-    cd cloudflare-core
-    export CF_ACCESS_CLIENT_ID=$(pulumi stack output service_token_id --show-secrets)
-    export CF_ACCESS_CLIENT_SECRET=$(pulumi stack output service_token_secret --show-secrets)
-)
+cd cloudflare-core
+export CF_ACCESS_CLIENT_ID=$(pulumi stack output service_token_id --show-secrets)
+export CF_ACCESS_CLIENT_SECRET=$(pulumi stack output service_token_secret --show-secrets)
+cd ..
 
 python -m nomad-proxy nomad.openttd.org
