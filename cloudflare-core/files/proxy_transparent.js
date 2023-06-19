7a4a2b1acfc20a2e6c4797f13b5e967b74c7257e
Patric Stout <truebrain@openttd.org>
2023-06-19 19:05:16 +0200

    fix(cloudflare-core): patch up broken GET requests so Cloudflare can proxy them

--- cloudflare-core/files/proxy_transparent.js
+++ cloudflare-core/files/proxy_transparent.js
@@ -2,12 +2,23 @@ export default {
     async fetch(request) {
         const url = new URL(request.url);
 
+        let headers = new Headers(request.headers);
+        let body = request.body;
+
+        /* Some tools, like GidGetHub, send a Content-Length header and an empty
+         * body on a GET request. This is not allowed, and so we remove both
+         * the header and the body here. */
+        if (request.method == "GET" && request.headers.has("content-length")) {
+            headers.delete("content-length");
+            body = undefined;
+        }
+
         /* Resend the request to the actual git host. */
         url.hostname = "[[ hostname ]]";
         const response = await fetch(url.toString(), {
             method: request.method,
-            headers: request.headers,
-            body: request.body,
+            headers,
+            body,
             redirect: "follow", // We follow redirects, to avoid us having to rewrite 307s etc.
         });
 
